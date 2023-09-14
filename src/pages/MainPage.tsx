60ba961ace23dfc024496d195268d9b408691526
mandarin-sep <vheh0901@gmail.com>
2023-09-15 00:49:18 +0900

    feat: "LocationSelector를 생성하면서 기본적인 zustand 적용"

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import Map from "../components/Map";
+import LocationSelector from "../components/LocationSelector";
+
+const MainPage = () => {
+  return (
+    <>
+      <Map />
+      <LocationSelector />
+    </>
+  );
+};
+
+export default MainPage;
