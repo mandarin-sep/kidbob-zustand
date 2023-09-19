f2a8779293eab902e6eb89c154668b3fd6b5cced
mandarin-sep <vheh0901@gmail.com>
2023-09-19 21:21:16 +0900

    feat: "가게 위치 마커로 표시"

---
+++ src/components/MapSection.tsx
@@ -0,0 +1,13 @@
+import Map from "./Map";
+import Markers from "./Markers";
+
+const MapSection = () => {
+  return (
+    <>
+      <Map />
+      <Markers />
+    </>
+  );
+};
+
+export default MapSection;
--- src/components/Markers.tsx
+++ src/components/Markers.tsx
@@ -1,31 +1,38 @@
 import { useCurrentShop } from "../store/useCurrentShop";
 import { useFetchShop } from "../store/useFetchShop";
+import { useLocationInfo } from "../store/useLocationInfo";
+import { useMap } from "../store/useMap";
+import Marker from "./Marker";
 
 const Markers = () => {
   const { shops } = useFetchShop();
   const { updateCurrentShop } = useCurrentShop();
+  const { division } = useLocationInfo();
+  const { map } = useMap();
 
-  if (!shops) return null;
+  if (!shops || !map) return null;
   return (
     <>
-      {shops.map((store) => {
-        return (
-          <Marker
-            map={map}
-            coordinates={
-              new window.naver.maps.LatLng(store.shopLat, store.shopLon)
-            }
-            onClick={() => {
-              updateCurrentShop({
-                shopBsType: store.shopBsType,
-                shopName: store.shopName,
-                shopRoadAddr: store.shopRoadAddr,
-              });
-            }}
-            key={store.shopId}
-          />
-        );
-      })}
+      {shops
+        .filter((item) => item.shopAddr.split(" ")[0] === division)
+        .map((store) => {
+          return (
+            <Marker
+              map={map}
+              coordinates={
+                new window.naver.maps.LatLng(store.shopLat, store.shopLon)
+              }
+              onClick={() => {
+                updateCurrentShop({
+                  shopBsType: store.shopBsType,
+                  shopName: store.shopName,
+                  shopRoadAddr: store.shopRoadAddr,
+                });
+              }}
+              key={store.shopId}
+            />
+          );
+        })}
     </>
   );
 };
